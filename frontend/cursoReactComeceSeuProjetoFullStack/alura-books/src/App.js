3c21ab3bf2d1264d8f4e743d2f55d71b1f0baf9f
Wallace Goulart <75147050+wallacegoulart@users.noreply.github.com>
2025-01-28 23:10:44 +0000

    Continuação do curso

--- frontend/cursoReactComeceSeuProjetoFullStack/alura-books/src/App.js
+++ frontend/cursoReactComeceSeuProjetoFullStack/alura-books/src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
 import Logo from './componentes/Logo'
+import perfil from './image/perfil.svg'
+import sacola from './image/sacola.svg'
+
+
+const textoListaItem =['CATEGORIAS','MINHA ESTANTE','FAVORITOS'];
+const icone= [perfil,sacola]
+
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'> 
         <Logo></Logo>
+        
+        <ul className='lista__primary'>
+          {textoListaItem.map( (texto)=>(
+            <li className='lista__item'><p>{texto}</p></li>
+          ) )}
+        </ul>
+        
+        <ul className='lista__secundaria'>
+          {icone.map( (icone)=>(
+            <li className='lista__item__icone'><p><img src={icone}></img></p></li>
+          )  )}
+        </ul>
+
+
       </header>
     </div>
   );
