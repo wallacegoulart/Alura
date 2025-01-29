fee3ad474535f8e9e95c9a9c2c0db7083f7aaed3
Wallace Goulart <75147050+wallacegoulart@users.noreply.github.com>
2025-01-29 21:44:11 +0000

    Usando a biblioteca FS

--- backend/JavaScriptNodeCriandoPrimeiraBiblioteca/app.js
+++ backend/JavaScriptNodeCriandoPrimeiraBiblioteca/app.js
@@ -1,1 +1,11 @@
-console.log("Teste");
+const fs = require('fs');
+
+const caminhoArquivo = process.argv;
+const link = caminhoArquivo[2];
+
+fs.readFile(link,'utf-8',(err,texto) => {
+    if(err){throw err;}
+    console.log(texto);
+})
+
+console.log(link);
