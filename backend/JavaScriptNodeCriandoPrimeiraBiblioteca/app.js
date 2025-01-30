813636f78ed28f01a04111b604e74b8c2ce65bed
Wallace Goulart <75147050+wallacegoulart@users.noreply.github.com>
2025-01-30 22:51:09 +0000

    Continuacao do curso de JavaScript

--- backend/JavaScriptNodeCriandoPrimeiraBiblioteca/app.js
+++ backend/JavaScriptNodeCriandoPrimeiraBiblioteca/app.js
@@ -5,8 +5,45 @@ const link = caminhoArquivo[2];
 
 fs.readFile(link,'utf-8',(err,texto) => {
     if(err){throw err;}
-    console.log(texto);
-    console.log("Concluido com sucesso")
+    quebraParagrafos(texto);
 })
+//1) Primeiro problema (video 01)
+// pegar o texto splitando pelo espaço e jogar num array 
+// pegar essa palavra e jogar dentro de um objeto, se repetir ele conta 
 
-console.log(link);
+//2) Segundo problema (video 02)
+// pensar num jeito para quebrar o texto em paragrafos para nao colocar no meu dic o '\n'
+// pegar todo texto tirar a diferenciação de maiuscula e minuscula
+
+//3) Terceiro problema (video 03)
+// retirar os caracteres especiais 
+// retirar as plavras curtas ( a, o ,e )
+
+
+function limparPalavra(palavra){
+    return  palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g ,'');
+}
+
+
+function quebraParagrafos(texto){
+    const listaParagrafos = texto.toLowerCase().split('\n');
+    const novoDicPalavras =  listaParagrafos.flatMap((paragrafos) =>{
+        if(!paragrafos){return[]};
+        return criacaoObejtoContandoPalavras(paragrafos); 
+    });
+    console.log( novoDicPalavras)
+}
+
+
+function criacaoObejtoContandoPalavras(texto){
+    const listaPalavras = texto.split(' ');
+    const dicPalavras = {};
+    listaPalavras.forEach(palavra => {
+        if(palavra.length >3){
+            const palavraLimpa_02 = limparPalavra(palavra)
+            dicPalavras[palavraLimpa_02] = (dicPalavras[palavraLimpa_02] || 0)+1;
+        };
+    })
+
+    return dicPalavras ;
+}
