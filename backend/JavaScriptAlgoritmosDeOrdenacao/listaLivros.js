382149cc2dd6edc877ef8e974e39efe43a803425
Wallace Goulart <75147050+wallacegoulart@users.noreply.github.com>
2025-03-12 00:05:50 +0000

    Atualizando o curso de JavaScript
    Signed-off-by: GitHub <noreply@github.com>

---
+++ backend/JavaScriptAlgoritmosDeOrdenacao/listaLivros.js
@@ -0,0 +1,28 @@
+const precosLivros = [
+  {
+    titulo: "JavaScript",
+    precos: 30,
+  },
+  {
+    titulo: "PHP",
+    precos: 15,
+  },
+  {
+    titulo: "Java",
+    precos: 40,
+  },
+  {
+    titulo: "Go",
+    precos: 50,
+  },
+  {
+    titulo: "Elixir",
+    precos: 45,
+  },
+  {
+    titulo: "Python",
+    precos: 20,
+  },
+];
+
+module.exports = precosLivros;
